428d0c17628d1a082a091e382cd56392c2314860
Fabian Mahdi <fabian@mahdi.at>
2023-11-25 11:33:51 +0100

    ldapmanagaer better errors

--- Server/src/ldapmanager.cpp
+++ Server/src/ldapmanager.cpp
@@ -4,18 +4,28 @@ LDAPManager::LDAPManager()
 {
     ldapUri = "ldap://ldap.technikum-wien.at:389";
     ldapVersion = LDAP_VERSION3;
+    int rc;
 
     // setup LDAP connection
-    if(ldap_initialize(&ldapHandle, &ldapUri[0]) != LDAP_SUCCESS)
-        throw std::runtime_error("ldap_init failed");
+    if((rc = ldap_initialize(&ldapHandle, &ldapUri[0])) != LDAP_SUCCESS)
+    {
+        std::string error = ldap_err2string(rc);
+        throw std::runtime_error("ldap_init failed: " + error);
+    }
 
     // set LDAP version
-    if(ldap_set_option(ldapHandle, LDAP_OPT_PROTOCOL_VERSION, &ldapVersion) != LDAP_SUCCESS)
-        throw std::runtime_error("failed to set ldap version");
+    if((rc = ldap_set_option(ldapHandle, LDAP_OPT_PROTOCOL_VERSION, &ldapVersion)) != LDAP_SUCCESS)
+    {
+        std::string error = ldap_err2string(rc);
+        throw std::runtime_error("failed to set ldap version: " + error);
+    }
 
     // initialize tls
-    if(ldap_start_tls_s(ldapHandle, nullptr, nullptr) != LDAP_SUCCESS)
-        throw std::runtime_error("failed to start tls (ldap)");
+    if((rc = ldap_start_tls_s(ldapHandle, nullptr, nullptr)) != LDAP_SUCCESS)
+    {
+        std::string error = ldap_err2string(rc);
+        throw std::runtime_error("failed to start tls (ldap): " + error);
+    }
 }
 
 // public functions
